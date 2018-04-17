1f2fc238871c606693c7b4b750ca0c5674b852ef
Oleg Gudzii <oleg198679@gmail.com>
2018-04-17 13:03:09 +0300

    Small refactoring

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -2,5 +2,5 @@
 
 import {greetUser} from '..';
 
-console.log('Welcome to the Brain Games!');
+console.log('Welcome to the Brain Games!\n');
 greetUser();
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import readlineSync from 'readline-sync';
 
 export const greetUser = () => {
-  console.log('');
   const gamerName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${gamerName}!`);	
 };
