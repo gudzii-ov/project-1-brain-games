46cdd00fa80f975c7f93c92bddf986b16c0fb70d
Oleg Gudzii <oleg198679@gmail.com>
2018-04-19 09:28:54 +0300

    Add new game - even-game

--- src/index.js
+++ src/index.js
@@ -1,8 +1,38 @@
 import readlineSync from 'readline-sync';
 
-const greetUser = () => {
-  const gamerName = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${gamerName}!`);
+export const greetUser = () => {
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!\n`);
+  return userName;
 };
 
-export default greetUser;
+const isEvenAnswer = (number) => {
+  if (number % 2 === 0) {
+    return 'yes';
+  }
+  return 'no';
+};
+
+export const evenGame = (userName) => {
+  const iter = (successTries) => {
+    if (successTries === 3) {
+      console.log(`Congratulations, ${userName}`);
+      return;
+    }
+
+    const randomNumber = Math.random();
+    console.log(`Question: ${randomNumber}`);
+    const rightAnswer = isEvenAnswer(randomNumber);
+    const userAnswer = readlineSync.question('Your answer: ');
+
+    if (userAnswer === rightAnswer) {
+      console.log('Correct!');
+      iter(successTries + 1);
+    } else {
+      console.log(`${userAnswer} is wrong answer ;(. Correct answer was ${rightAnswer}.\n`);
+      console.log(`Let's try again, ${userName}!`);
+    }
+  };
+
+  iter(0);
+};
